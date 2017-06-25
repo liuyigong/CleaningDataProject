d6b321d7e240bb12516a91353b8489c39be93ebf
Yigong Liu <liuagong@gmail.com>
2017-06-25 15:18:57 -0400

    Code to clean and generate AveData

---
+++ run_analysis.R
@@ -0,0 +1,18 @@
+library(dplyr)
+train<-cbind(read.table("UCI HAR Dataset/train/subject_train.txt",col.names = "subject"),read.table("UCI HAR Dataset/train/y_train.txt",col.names = "activity"),read.table("UCI HAR Dataset/train/X_train.txt"))
+test<-cbind(read.table("UCI HAR Dataset/test/subject_test.txt",col.names = "subject"),read.table("UCI HAR Dataset/test/y_test.txt",col.names = "activity"),read.table("UCI HAR Dataset/test/X_test.txt"))
+features<-read.table("UCI HAR Dataset/features.txt")
+label<-read.table("UCI HAR Dataset/activity_labels.txt")
+SumData <- rbind(train,test)
+names(SumData)[3:length(names(SumData))]<-as.vector(features[,2])
+SumData<-cbind(SumData[,1:2],SumData[,grepl("mean\\()|std()",names(SumData))])
+SumData$activity<-factor(SumData$activity,labels = as.vector(label[,2]))
+names(SumData)[grepl("mean",names(SumData))]<- paste("mean of",names(SumData)[grepl("mean",names(SumData))])
+names(SumData)[grepl("std",names(SumData))]<- paste("std of",names(SumData)[grepl("std",names(SumData))])
+names(SumData)<-gsub("-.+\\)","",names(SumData))
+AveSumData <- SumData %>%
+  group_by(subject,activity) %>%
+  summarize_all(funs(mean))
+names(AveSumData)[3:length(names(AveSumData))]<-paste("average",names(SumData)[3:length(names(AveSumData))])
+rm(train,test,features,label)
+write.table(AveSumData,file = "AveData.txt", row.names = FALSE)
